5ed8633e4ef4e51a7787565d5db768646ca4330f
Tommy-Graham <134443039+Tommy-Graham@users.noreply.github.com>
2023-09-24 19:24:24 +0100

    Created GET requests and tests

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,4 +5,8 @@ const artistRouter = express.Router();
 
 artistRouter.post('/', controllerArtist.createArtist);
 
+artistRouter.get('/', controllerArtist.getAllArtists);
+
+artistRouter.get('/:id', controllerArtist.getById);
+
 module.exports = artistRouter;
